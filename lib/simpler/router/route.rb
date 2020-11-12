7150fc69a268bd33574249c1b22aead5e602a45e
Alexander Chaschin <chaschin@uchi.ru>
2020-11-13 00:53:56 +0300

    add path regexp

--- lib/simpler/router/route.rb
+++ lib/simpler/router/route.rb
@@ -6,13 +6,23 @@ module Simpler
 
       def initialize(method, path, controller, action)
         @method = method
-        @path = path
+        @path = make_path(path)
         @controller = controller
         @action = action
       end
 
       def match?(method, path)
-        @method == method && path == @path
+        @method == method && @path =~ path
+      end
+
+      private
+
+      def make_path(path)
+        regex_s = path.split('/')
+          .map { |part| part.start_with?(':') ? "(?<#{part[1..]}>[a-z0-9]+)" : part }
+          .join('/') + '$'
+
+        Regexp.new(regex_s, Regexp::IGNORECASE)
       end
 
     end
